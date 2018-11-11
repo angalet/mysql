df7832ca3b8dbf3c26517771e023bab1731fe190
angalet <angaleit@gmail.com>
2018-11-11 16:23:27 +0300

    first commit

--- 2/index.php
+++ 2/index.php
@@ -16,17 +16,22 @@
 </html>
 <?php
 $pdo = new PDO("mysql:host=localhost;dbname=netology01; charset=utf8","admin","1qa2ws3ed");
-$user_from_base = "SELECT id,login,password FROM user WHERE login= ?";
+//$user_from_base = "SELECT id,login,password FROM user WHERE login= ?";
 session_start();
 if (isset($_POST["auth"])){
 if (!isset($_SESSION['NAME']) and isset($_SERVER['PHP_AUTH_USER'])) {
-    $user_from_base = "SELECT id,login FROM user WHERE login= ?";
+    echo $_SESSION['NAME']." на авторизацию<br>";
+    $user_from_base = "SELECT id,login,password FROM user WHERE login= ?";
     //$sql = "INSERT INTO user (login, password) VALUES (:login, :password)";
     $stmt= $pdo->prepare($user_from_base);
     $stmt->execute([$_SERVER['PHP_AUTH_USER']]);
     $login = $stmt->fetch();
-    //print_r($login)."<br>";
-    if ($_SERVER['PHP_AUTH_USER'] and $_SERVER['PHP_AUTH_USER']==$login["login"] and $_SERVER['PHP_AUTH_PWD']==$login["password"]){
+    echo "<pre>";
+    print_r($login);
+    print_r($_SERVER);
+    print_r($_SESSION);
+    echo "</pre>";
+    if ($_SERVER['PHP_AUTH_USER'] and $_SERVER['PHP_AUTH_USER']==$login["login"] and $_SERVER['PHP_AUTH_PW']==$login["password"]){
         $_SESSION['NAME'] = $login["login"];
         $_SESSION['user_id'] = $login["id"];
         setcookie("user_name", $_SERVER['PHP_AUTH_USER']);
@@ -61,10 +66,10 @@ if (isset($_POST["reg"])){
     
     }
 }
-echo $_SESSION['NAME'];
-echo $_SESSION['user_id'];
+//echo $_SESSION['NAME']."<br>";
+echo $_SESSION['user_id']." user_id<br>";
 if ($_SESSION['NAME']){
-    echo " Вы авторизовались<br>";
+    echo $_SESSION['NAME']." Вы авторизовались<br>";
     ?>
     <form method="post">
         <p><input type="text" name="task" value="" /> Дело</p>
@@ -80,6 +85,53 @@ if ($_SESSION['NAME']){
         $stmt= $pdo->prepare($sql);
         $stmt->execute($data);
     }
+    echo "====================================<br>";
+    ?>
+    <form method='post'>
+    <table width="40%" border="1" >
+<tr>
+    <th>удалить</th>
+    <th>Дело</th>
+   	<th>Когда</th>
+   </tr>
+   <?php
+    $data = [
+        'user_id' => $_SESSION['user_id']
+    ];
+    $sql = "SELECT id, description, date_added FROM task WHERE user_id=user_id ORDER BY date_added ";
+    $stmt= $pdo->prepare($sql);
+    $stmt->execute($data); //$_SESSION['user_id']
+    $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    //print_r($tasks);
+    foreach ($tasks as $row){
+        echo "<tr>
+            <td><input type='checkbox' name='gelete_row[]' value=".$row['id']."> ".$row['id']."</td>
+            <td>".$row['description']."</td>
+            <td>".date("Y M d",strtotime($row['date_added']))."</td>
+            </tr>";
+    }
+    ?>
+    </table>
+    <p><input type="submit" name="gelete_row_submit" value="OK" > удалить выбранное</p>
+    </form>
+    <?php
+    if (isset($_POST['gelete_row_submit']) and isset($_POST['gelete_row']) ){
+        print_r($_POST['gelete_row']);
+        $id_task - NULL;
+        $id_task = implode(',',$_POST['gelete_row']);
+        echo $id_task;
+        $data = [
+            'id_task' => $id_task
+        ];
+        $sql = "DELETE FROM task WHERE id IN (".$id_task.")";
+        //$sql = "DELETE FROM task WHERE id IN (:$id_task)";
+        echo $sql."<br>";
+        $stmt= $pdo->prepare($sql);
+        $stmt->execute(); //тут почему то не работает через $data
+        echo $sql;
+
+    }
+
 
 }
 
